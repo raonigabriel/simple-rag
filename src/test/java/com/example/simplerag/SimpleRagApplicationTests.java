f61c4413e343b8705903430449027a769c0e4e4f
raonigabriel <raonigabriel@gmail.com>
2025-03-07 09:26:36 -0300

    chore: renamed class

--- src/test/java/com/example/simplerag/SimpleRagApplicationTests.java
+++ src/test/java/com/example/simplerag/SimpleRagApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimpleragApplicationTests {
+class SimpleRagApplicationTests {
 
 	@Test
 	void contextLoads() {
